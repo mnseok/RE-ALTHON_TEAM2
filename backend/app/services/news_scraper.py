fb5b6ee304aab17c3af2bcbcd7dc9bdbbde5569d
Seobin Choi <78406242+emptybinnn@users.noreply.github.com>
2024-12-07 01:08:30 +0900

    venv done man....

--- backend/app/services/news_scraper.py
+++ backend/app/services/news_scraper.py
@@ -2,8 +2,10 @@ import requests
 from app.config import Config
 
 def fetch_news(source="bbc-news"):
-    url = f"https://newsapi.org/v2/top-headlines?sources={source}&apiKey={Config.NEWS_API_KEY}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    return {"error": "Failed to fetch news"}
+    # url = f"https://newsapi.org/v2/top-headlines?sources={source}&apiKey={Config.NEWS_API_KEY}"
+    # response = requests.get(url)
+    # if response.status_code == 200:
+    #     return response.json()
+    # return {"error": "Failed to fetch news"}
+    print("requset")
+    return 0
